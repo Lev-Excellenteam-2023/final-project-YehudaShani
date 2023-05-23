5310bf176320472d8e7491f4e7ab8fcdbfe172de
yehuda shani <yehudashani88@gmail.com>
2023-05-23 11:34:28 +0300

    added get_text_from_slide method, still needs to fix bold letters problem

--- parser.py
+++ parser.py
@@ -3,6 +3,9 @@ import collections.abc
 from pptx import Presentation
 
 
+
+
+
 class Parser:
 
     def __init__(self, path):
@@ -12,6 +15,15 @@ class Parser:
 
     def __iter__(self):
         return self.slides.__iter__()
+
+    def get_text_from_slide(self, slide):
+        text_runs = []
+        for shape in slide.shapes:
+            if not shape.has_text_frame:
+                continue
+            for paragraph in shape.text_frame.paragraphs:
+                text_runs.append(paragraph.text)
+        return text_runs
 
 
 
