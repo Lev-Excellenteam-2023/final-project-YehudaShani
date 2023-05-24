b1c76e184a327f1e42c4dad7a60a2e4fc857517a
yehuda shani <yehudashani88@gmail.com>
2023-05-24 11:55:20 +0300

    program works, but slow, think asyncio should be helping doing paralel tasks

--- gpt_prompter.py
+++ gpt_prompter.py
@@ -1,9 +1,7 @@
 # secret key is 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
-from typing import List
 
 import openai
 import asyncio
-
 
 from parser import Parser, get_text_from_slide
 
@@ -12,8 +10,12 @@ openai.api_key = "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"
 
 def generate_summary(parser: Parser):
     messages_to_send = []
-    messages_to_send.append({"role": "assistant", "content": "you are a chatbot that explains bullet points"})
+    messages_to_send.append({"role": "assistant", "content": "you are a chatbot that explains bullet points in a "
+                                                             "powerpoint presentation to a student"})
     for slide in parser:
+        get_text_from_slide = get_text_from_slide(slide)
+        if len(get_text_from_slide) == 0:
+            continue
         content = " ".join(get_text_from_slide(slide))
         messages_to_send.append({"role": "user", "content": content})
 
@@ -23,19 +25,16 @@ def generate_summary(parser: Parser):
         )
 
         chat_response = completion.choices[0].message.content
-        print(f'ChatGPT: {chat_response}')
         messages_to_send.append({"role": "assistant", "content": chat_response})
 
         yield chat_response
-        #wait 20 seconds
+
 
 async def get_gpt_answer(parser):
     generator = generate_summary(parser)
     while True:
         try:
-            print(next(generator))
+            yield next(generator)
             await asyncio.sleep(20)
         except StopIteration:
             break
-
-
