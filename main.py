2244db042780865bb833f2a9e55cc65c1f1318d2
yehuda shani <yehudashani88@gmail.com>
2023-06-12 16:00:26 +0300

    added main out of package, key works, need to fix asyncio

---
+++ main.py
@@ -0,0 +1,23 @@
+# Author: Yehuda Shani
+# Date: 20/5/2021
+# Program description: This program is the main program that runs the GPT-3 model on powerpoint presentations.
+# and writes the summary to a JSON file.
+
+from Explainer import parser
+from Explainer import gpt_prompter
+import os
+import asyncio
+import glob
+
+
+def main():
+    uploads_path = "content/uploads/"
+    pptx_files = [os.path.join(uploads_path, file) for file in os.listdir(uploads_path) if file.endswith('.pptx')]
+
+    for file in pptx_files:
+        my_parser = parser.Parser(file)
+        asyncio.run(gpt_prompter.get_gpt_answer(my_parser, file))
+
+
+if __name__ == "__main__":
+    main()
