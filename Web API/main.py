ba918adad4b99eaf7a5f767a7ab858eadcef13ab
yehuda shani <yehudashani88@gmail.com>
2023-06-13 20:02:18 +0300

    status is working, might need to change 404 error

--- Web API/main.py
+++ Web API/main.py
@@ -5,15 +5,19 @@ from flask import Flask, render_template, request
 from datetime import datetime
 import os
 import uuid
+import json
 
 app = Flask(__name__, template_folder='templates')
 
 app.config['UPLOAD_FOLDER'] = 'C:\\Users\\yehuda\\Documents\\ExcelenTeam\\Python\\Exercises\\GPT API\\content\\uploads'
+app.config['OUTPUT_FOLDER'] = 'C:\\Users\\yehuda\\Documents\\ExcelenTeam\\Python\\Exercises\\GPT API\\content\\outputs'
+
 
 # Route for the home page
 @app.route('/')
 def home():
     return render_template('home.html')
+
 
 # Route for handling file uploads
 @app.route('/upload', methods=['POST'])
@@ -26,20 +30,33 @@ def upload_file():
         file_id = generate_id()
         filename_to_save = '-'.join([file_name_without_extension, timestamp, file_id]) + file_name_extension
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename_to_save))
-        return render_template('home.html', file_id=file_id)
+        return render_template('home.html', file_id=json.dumps({"uid": file_id}))
 
     else:
         return 'No file uploaded.'
 
+
 # Route for checking upload status
 @app.route('/status', methods=['POST'])
 def check_status():
-    # Handle the upload status check logic
-    # ...
-    pass
+    # get file id from request
+    file_id = request.form['upload_id']
+    # check if file exists
+    for file in os.listdir(app.config['OUTPUT_FOLDER']):
+        file_ending = file_id + '.json'
+        if file.endswith(file_ending):
+            # check if file has only '['
+            with open(os.path.join(app.config['OUTPUT_FOLDER'], file), 'r') as f:
+                if f.read() == '' or f.read() == '[':
+                    json_answer = json.dumps({"status": "processing"})
+                else:
+                    json_answer = json.dumps({"status": "ready"})
+
+    return render_template('home.html', status_answer=json_answer)
+
 
 def generate_id():
-    # generate id using uuid
+    # generate json file id
     return str(uuid.uuid4())
 
 
